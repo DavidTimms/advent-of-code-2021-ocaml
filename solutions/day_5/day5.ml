3d7a7bbc629c1bd54ab52c0269b506fa68cfa46f
David Timms <davidtimms@btinternet.com>
2021-12-27 14:59:00 +0000

    Completed day 5, part 2

--- solutions/day_5/day5.ml
+++ solutions/day_5/day5.ml
@@ -27,15 +27,17 @@ type vent_line = {
 
 type point_frequency_map = (Point.t, int, Point.comparator_witness) Map.t
 
-(* let tap f value =
+(*
+let tap f value =
   f value;
-  value *)
+  value
 
-(* let print_point (x, y) =
-  Stdio.printf "(%i,%i)\n" x y *)
+let print_point (x, y) =
+  Stdio.printf "(%i,%i)\n" x y
 
-(* let print_vent_line { start = (x1, y1); finish = (x2, y2) } =
-  Stdio.printf "(%i,%i) -> (%i,%i)\n" x1 y1 x2 y2 *)
+let print_vent_line { start = (x1, y1); finish = (x2, y2) } =
+  Stdio.printf "(%i,%i) -> (%i,%i)\n" x1 y1 x2 y2
+*)
 
 let parse_vent_line coordinates_string =
   let regex = Str.regexp "^\\([0-9]+\\),\\([0-9]+\\) -> \\([0-9]+\\),\\([0-9]+\\)$" in
@@ -53,33 +55,18 @@ let input =
   |> String.split_lines
   |> List.map ~f:parse_vent_line
 
-let is_line_horizontal { start = (_, y1); finish = (_, y2) } =
-  y1 = y2
-
-let is_line_vertical { start = (x1, _); finish = (x2, _) } =
-  x1 = x2
-
 let is_line_diagonal { start = (x1, y1); finish = (x2, y2) } =
   x1 <> x2 && y1 <> y2
 
 let ignore_diagonal_lines =
   List.filter ~f:(Fn.non is_line_diagonal)
 
-let horizontal_line_to_points { start = (x1, y1); finish = (x2, _) } =
-  List.range ~start:`inclusive ~stop:`inclusive (Int.min x1 x2) (Int.max x1 x2)
-  |> List.map ~f:(fun x -> (x, y1))
-
-let vertical_line_to_points { start = (x1, y1); finish = (_, y2) } =
-  List.range ~start:`inclusive ~stop:`inclusive (Int.min y1 y2) (Int.max y1 y2)
-  |> List.map ~f:(fun y -> (x1, y))
-
-let line_to_points line =
-  if is_line_horizontal line then
-    horizontal_line_to_points line
-  else if is_line_vertical line then
-    vertical_line_to_points line
-  else
-    raise (Failure "Diagonal lines are not  supported")
+let line_to_points { start = (x1, y1); finish = (x2, y2) } =
+  let length = Int.max (Int.abs (x1 - x2)) (Int.abs (y1 - y2)) in
+  let x_direction = Int.compare x2 x1 in
+  let y_direction = Int.compare y2 y1 in
+  List.range ~start:`inclusive ~stop:`inclusive 0 length
+  |> List.map ~f:(fun i -> (x1 + (i * x_direction), y1 + (i * y_direction)))
 
 let count_points: Point.t list -> point_frequency_map =
     List.fold ~init:(Map.empty (module Point)) ~f:(fun frequency_map point ->
@@ -102,4 +89,7 @@ let part1 =
   |> find_most_dangerous_points
   |> List.length
 
-let part2 = 0
+let part2 =
+  input
+  |> find_most_dangerous_points
+  |> List.length
